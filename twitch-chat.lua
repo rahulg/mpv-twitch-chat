7b051b39a8087e1e3906bb905f04895525d9e78f
Rahul AG <r@hul.ag>
2016-06-08 16:12:01 +0800

    add keybinds and options
    don't display a name for system messages
    colour streamer messages differently

--- twitch-chat.lua
+++ twitch-chat.lua
@@ -1,11 +1,30 @@
 require 'luarocks.loader'
 local https = require 'ssl.https'
 local json = require 'lunajson'
+local options = require 'mp.options'
 
 local osd_ass_cc = mp.get_property_osd('osd-ass-cc/0')
-local username
 local chat
-local tm_tick, tm_redraw
+
+local opt = {
+	toggle_key = 'c',
+	enable_position_binds = true,
+	osd_position = 1, -- osd_position uses 'numpad values'
+	message_duration = 10, -- in seconds
+	message_limit = 10,
+	update_interval = 0.3,
+	redraw_interval = 0.5,
+
+	-- text styling
+	font = 'Source Sans Pro',
+	font_size = 10,
+	font_colour = 'FFFFFF',
+	border_size = 1.0,
+	border_colour = '000000',
+	alpha = '11',
+	streamer_font_colour = 'FF0000'
+}
+options.read_options(opt)
 
 local Deque = {}
 Deque.__index = Deque
@@ -80,10 +99,11 @@ end
 local TwitchChat = {}
 TwitchChat.__index = TwitchChat
 
-function TwitchChat.new(video_id)
+function TwitchChat.new(streamer, video_id)
 	local self = setmetatable({}, TwitchChat)
 
 	self.video_id = video_id
+	self.streamer = streamer
 	self.messages = Deque.new()
 	self.display = Deque.new()
 
@@ -112,7 +132,7 @@ function TwitchChat:fetch_block(ts_absolute)
 		for _, msg in ipairs(resp.data) do
 			local message = {}
 			message.ts = (msg.attributes.timestamp / 1000) - self.ts_start
-			message.user = msg.attributes.tags['display-name'] or 'system message'
+			message.user = msg.attributes.tags['display-name']
 			message.text = msg.attributes.message
 			self.messages:rpush(message)
 		end
@@ -130,21 +150,14 @@ function TwitchChat:fetch_block_at_ts(ts)
 	self:fetch_block(self.ts_start + ts)
 end
 
-function TwitchChat:next_message()
-	while self.messages:length() == 0 do
-		self:fetch_next_block()
-	end
-	return self.messages:lpop()
-end
-
 function TwitchChat:update_display(ts)
-	while not self.display:empty() and self.display:lpeek().ts < ts - 10 do
+	while not self.display:empty() and self.display:lpeek().ts < ts - opt.message_duration do
 		self.display:lpop()
 	end
-	while (self.messages):length() == 0 do
+	if self.messages:empty() then
 		self:fetch_next_block()
 	end
-	while not self.messages:empty() and self.messages:lpeek().ts < ts - 10 do
+	while not self.messages:empty() and self.messages:lpeek().ts < ts - opt.message_duration do
 		self.messages:lpop()
 	end
 	while not self.messages:empty() and self.messages:lpeek().ts < ts do
@@ -152,10 +165,25 @@ function TwitchChat:update_display(ts)
 	end
 end
 
-function show_chat()
+function has_vo()
 	local vo_conf = mp.get_property("vo-configured")
 	local video = mp.get_property("video")
 	return vo_conf == "yes" and (video and video ~= "no" and video ~= "")
+end
+
+function txt_username(s)
+	if s == nil then
+		return ''
+	end
+	if s:lower() == chat.streamer:lower() then
+		s = string.format(
+			'{\\1c&H%s&}%s{\\1c&H%s&}',
+			opt.streamer_font_colour,
+			s,
+			opt.font_colour
+		)
+	end
+	return string.format('{\\b1}%s:{\\b0} ', s)
 end
 
 function ev_tick()
@@ -167,37 +195,63 @@ function ev_tick()
 end
 
 function ev_redraw()
-	if not show_chat() then
-		return
-	end
-	if chat == nil or chat.display:empty() then
-		return
-	end
-	local message = string.format(
-		'%s{\\fs%d}{\\fn%s}',
+	if chat == nil or chat.display:empty() then return end
+	local message = ''
+	if not has_vo() then return end
+	message = string.format(
+		'%s{\\an%d}{\\fs%d}{\\fn%s}{\\bord%f}{\\3c&H%s&}{\\1c&H%s&}{\\alpha&H%s&}',
 		osd_ass_cc,
-		10,
-		'Source Sans Pro'
+		opt.osd_position,
+		opt.font_size,
+		opt.font,
+		opt.border_size,
+		opt.border_colour,
+		opt.font_colour,
+		opt.alpha
 	)
 	for idx=chat.display.head, chat.display.tail do
+		if idx - chat.display.head == opt.message_limit then
+			chat.display:lpop()
+			break
+		end
 		local msg = chat.display[idx]
-		message = message .. string.format(
-			'{\\b1}%s:{\\b0}\\h\\h%s\\N',
-			msg.user,
+			message = message .. string.format(
+			'%s%s\\N',
+			txt_username(msg.user),
 			msg.text
 		)
 	end
-	mp.osd_message(message, 1.0)
+	mp.osd_message(message, opt.redraw_interval + 0.1)
 end
+
+local tm_tick = mp.add_periodic_timer(opt.update_interval, ev_tick)
+tm_tick:kill()
+local tm_redraw = mp.add_periodic_timer(opt.redraw_interval, ev_redraw)
+tm_redraw:kill()
+local tm_redraw_enabled = false
 
 function ev_pause(name, paused)
 	if paused == true then
 		tm_tick:stop()
-		tm_redraw:stop()
 	else
 		tm_tick:resume()
-		tm_redraw:resume()
 	end
+end
+
+function ev_toggle()
+	if tm_redraw_enabled then
+		tm_redraw:kill()
+		tm_redraw_enabled = false
+		mp.osd_message('', 0)
+	else
+		tm_redraw:resume()
+		tm_redraw_enabled = true
+		ev_redraw()
+	end
+end
+
+function ev_set_pos(position)
+	opt.osd_position = position
 end
 
 function ev_seek()
@@ -213,15 +267,34 @@ function ev_start_file()
 	if path == nil then
 		return
 	end
-	local pat_twitch_vod = 'twitch.tv/.-/v/(%d+)'
-	local video_id = string.match(path, pat_twitch_vod)
+	local pat_twitch_vod = 'twitch.tv/(.-)/v/(%d+)'
+	local streamer, video_id = string.match(path, pat_twitch_vod)
 	if video_id ~= nil then
-		chat = TwitchChat.new(video_id)
+		chat = TwitchChat.new(streamer, video_id)
+		tm_tick:resume()
+		tm_redraw:resume()
+		tm_redraw_enabled = true
 		mp.register_event('playback-restart', ev_seek)
-		mp.observe_property("pause", "bool", ev_pause)
-		tm_tick = mp.add_periodic_timer(0.5, ev_tick)
-		tm_redraw = mp.add_periodic_timer(1.0, ev_redraw)
+		mp.observe_property('pause', 'bool', ev_pause)
+		mp.add_key_binding(opt.toggle_key, 'toggle', ev_toggle, {repeatable=false})
 	end
 end
 
+function ev_end_file()
+	if tm_tick ~= nil then tm_tick:kill() end
+	if tm_redraw ~= nil then tm_redraw:kill() end
+	tm_redraw_enabled = false
+	tm_tick = nil
+	tm_redraw = nil
+	mp.unregister_event(ev_seek)
+	mp.unobserve_property(ev_pause)
+	mp.remove_key_binding('toggle')
+end
+
 mp.register_event('start-file', ev_start_file)
+mp.register_event('end-file', ev_end_file)
+if opt.enable_position_binds then
+	for key=1, 9 do
+		mp.add_key_binding('KP' .. key, 'position-' .. key, function() ev_set_pos(key) end)
+	end
+end
